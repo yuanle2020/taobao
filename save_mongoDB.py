6c565ccb5cd7058134c2f933747d68deaa7e9508
yuan <591788925@qq.com>
2020-11-02 10:12:09 +0800

    数据存储mongodb

---
+++ save_mongoDB.py
@@ -0,0 +1,20 @@
+import pymongo
+
+# mongoDB
+host = '127.0.0.1'
+port = 27017
+set_name = "taobao"
+
+
+def save_mongoDB(res_list,table_name):
+    L_itemId = []
+    client = pymongo.MongoClient(host=host, port=port)
+    db = client[set_name]
+
+    for i in res_list:
+        db[table_name].update({'itemId': i['itemId']},{'$set':dict(i)},True)
+        print(i)
+        L_itemId.append(i['itemId'])
+
+    return L_itemId
+
