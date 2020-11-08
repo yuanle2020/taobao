8cf44d49b56057a512e45d4d86654d51f6f7eace
yuan <591788925@qq.com>
2020-11-08 20:29:11 +0800

    淘宝爬取

--- taobao2.5.1.py
+++ taobao2.5.1.py
@@ -1,6 +1,6 @@
 import requests
 import hashlib
-import time, os, csv
+import time, os, csv, json
 from urllib.parse import quote
 import threading
 from queue import Queue
@@ -8,7 +8,7 @@ from queue import Queue
 
 class TaoBao:
     def __init__(self, str_searchContent, num_pageSize, num_page, appKey, threads_num_get_pages,
-                 switch_save, proxies, threads_num_get_comments):
+                 switch_save, proxies, threads_num_get_comments, taobao_comments_num):
         self.str_searchContent = str_searchContent
         self.num_pageSize = num_pageSize
         self.num_page = num_page
@@ -17,6 +17,7 @@ class TaoBao:
         self.threads_num_get_comments = threads_num_get_comments
         self.switch_save = switch_save
         self.proxies = proxies
+        self.taobao_comments_num = taobao_comments_num
         self.cookie = ''
         self.token = ''
         self.file_name = ''
@@ -31,7 +32,6 @@ class TaoBao:
 
             response = requests.get(base_url)
             get_cookies = response.cookies.get_dict()
-            print(get_cookies)
             _m_h5_tk = get_cookies['_m_h5_tk']
             _m_h5_tk_enc = get_cookies['_m_h5_tk_enc']
             self.token = _m_h5_tk.split('_')[0]
@@ -49,7 +49,7 @@ class TaoBao:
     def second_requests(self):
         # 第二次带cookie请求,返回数据并存储
         searchContent = self.str_searchContent
-        pageSize = str(self.num_pageSize)  # 每页结果属
+        pageSize = str(self.num_pageSize)  # 每页结果数
         page = str(self.num_page)  # 第几页
         str_data = '{"pNum":' + page + ',"pSize":"' + pageSize + r'","refpid":"mm_26632258_3504122_32538762","variableMap":"{\"q\":\"' + searchContent + r'\",\"navigator\":false,\"union_lens\":\"recoveryid:201_11.20.207.176_1974284_1603940990104;prepvid:201_11.20.207.176_1974284_1603940990104\",\"recoveryId\":\"201_11.92.48.12_2098594_1603960009303\"}","qieId":"36308","spm":"a2e0b.20350158.31919782","app_pvid":"201_11.92.48.12_2098594_1603960009303","ctm":"spm-url:a2e0b.20350158.search.1;page_url:https%3A%2F%2Fuland.taobao.com%2Fsem%2Ftbsearch%3Frefpid%3Dmm_26632258_3504122_32538762%26keyword%3D%25E8%25A3%25A4%25E5%25AD%2590%26clk1%3D38b36e679f3fa0d5a81bf45a4bb89d18%26upsid%3D38b36e679f3fa0d5a81bf45a4bb89d18%26spm%3Da2e0b.20350158.search.1%26pid%3Dmm_26632258_3504122_32538762%26union_lens%3Drecoveryid%253A201_11.20.207.176_1974284_1603940990104%253Bprepvid%253A201_11.20.207.176_1974284_1603940990104"}'
 
@@ -66,9 +66,8 @@ class TaoBao:
         try:
             with requests.get(url, headers=headers) as res:
                 html = res.text
-                res_str = html.split(
-                    '"resultList":')[-1].split('}},"ret":')[0].replace('true', '"true"').replace('false', '"false"')
-                res_list = eval(res_str)
+                res_str = html.split('"resultList":')[-1].split('}},"ret":')[0]
+                res_list = json.loads(res_str)
                 if self.switch_save == 0:
                     self.switch_save_0(res_list)
                 elif self.switch_save == 1:
@@ -87,19 +86,19 @@ class TaoBao:
         # 返回该页面所有的 itemId 存入 L_itemId 列表中
         # print(res_list)
         self.L_itemId += save_csv(res_list, csv_file_name)
-        print('\n已爬取项目数目: ', len(self.L_itemId))
+        print('\n已完成爬取项目数目: ', len(self.L_itemId))
 
     # 保存进mysql数据库
     def switch_save_1(self, res_list):
         from save_mysql import save_mysql
         self.L_itemId += save_mysql(res_list, self.str_searchContent)
-        print('\n爬取项目数目: ', len(self.L_itemId))
+        print('\n已完成爬取项目数目: ', len(self.L_itemId))
 
     # 保存进mongodb数据库
     def switch_save_2(self, res_list):
         from save_mongoDB import save_mongoDB
         self.L_itemId += save_mongoDB(res_list, self.str_searchContent)
-        print('\n爬取项目数目: ', len(self.L_itemId))
+        print('\n已完成爬取项目数目: ', len(self.L_itemId))
 
     # 关键字搜索爬虫，调用第一/第二次请求
     def get_search_page(self):
@@ -117,6 +116,7 @@ class TaoBao:
         while n > 0:
             itemId = self.L_itemId.pop(0)
             try:
+                print(f'{threading.current_thread().name}开始爬取评论')
                 self.get_comments(itemId)
                 n = 3
             except Exception as e:
@@ -126,22 +126,35 @@ class TaoBao:
 
     # 请求评论的数据
     def get_comments(self, itemId):
-        print('开始爬取评论')
-        tmp = str(time.time()).split('.')
-        url = f'https://rate.tmall.com/list_detail_rate.htm?itemId={itemId}&spuId=1834646277&sellerId=2206843128012&order=3&currentPage=2&append=0&content=1&tagId=&posi=&picture=&groupId=&ua=098%23E1hvgQv7vOpvUvCkvvvvvjiWP2My6jiUR2qOzjthPmPWlj38RssUzjDnP2SO6jlERvhvCvvvphm%2BvpvBCU2ktepvvCKAEBYJ6ahtuk4%2BvpvEvUmIdvwvvvuVdvhvmpmv%2FPA5vvmUMuQCvvyvvOeU%2BvvvCwIgvpvhvvCvpv9CvhACUvGPjNoAdBeXjL4xfBuKHdBYLnsvV1OqrADn9Wma%2BfmtEpch1C6t%2BFIlBqev%2Bu0O5C60dbm65iI4wh7Q0fJ6W3CQog0HKfUpejEUkvhvC9hvpyP9s89Cvv9vvhh6A77bef9Cvm9vvhCvvvvvvvvvByOvvvHivvCVB9vv9LvvvhXVvvmCjvvvByOvvUhwvvhvC9mvphvvvvgCvvpvvPMMRvhvCvvvphmevpvhvvCCBUOCvvpv9hCv9vhvj3gGhDi%2FzYswzjA875R2uU9E9fq2gFSEAUHU1%2BDsvQOCvvNwzHi4zAmlRvhvCvvvphm%2BvpvEvvhb2MQvvELAdvhvhpov%2BbpfvvmOYdO3ob5aRfvgdvhvmpvCcHYJvvv9RgvCvCLwMHnaCaMwznQhhxS5CrApzVD44v%3D%3D&needFold=0&_ksTS={tmp[0]}_{tmp[1]}&callback=jsonp{int(tmp[1]) + 1}'
-        referer = f'https://detail.tmall.com/item.htm?id={itemId}'
-        headers = {'cookie': self.cookie, 'referer': referer, }
+        itemId, sellerId, isTmall = itemId  # itemId是一个元组（itemId，isTmall）
+        # 判断是否天猫
+        if isTmall == 'true':
+            url = f'https://rate.tmall.com/list_detail_rate.htm?itemId={itemId}&sellerId={sellerId}'
+            referer = f'https://detail.tmall.com/item.htm?id={itemId}'
+        else:
+            url = f'https://rate.taobao.com/feedRateList.htm?auctionNumId={itemId}&currentPageNum=1&pageSize={self.num_pageSize}'
+            referer = f'https://item.taobao.com/item.htm?id={itemId}'
+        # x5sec=7b22726174656d616e616765723b32223a223963366265636632636238303536383262633632636134386461353832366363434c666a6e763046454c6643785962457366444e6777453d227d;
+        headers = {
+            'cookie': 'x5sec=7b22726174656d616e616765723b32223a223963366265636632636238303536383262633632636134386461353832366363434c666a6e763046454c6643785962457366444e6777453d227d;',
+            'referer': referer,
+        }
+        proxies = self.proxies
         with requests.get(url, headers=headers) as res:
-            comments = res.text.split('"rateList":')[1].split(',"searchinfo"')[0].replace('true', '"true"').replace(
-                'false', '"false"').replace('null', '"null"')
-            comments_list = eval(comments)
+            # 判断是否天猫
+            if isTmall == 'true':
+                comments = res.text.split('"rateList":')[1].split(',"searchinfo"')[0]
+            else:
+                comments = res.text.split('"comments":')[1].split(',"currentPageNum"')[0]
+            comments_list = json.loads(comments)
+            # 保存数据
             self.save_comments(comments_list, itemId)
 
     # 保存评论的数据入mongoDB
     def save_comments(self, comments_list, itemId):
         from save_comment_mongDB import save_mongoDB
         save_mongoDB(comments_list, itemId)
-        print(f'{itemId}--> mongoDB入库成功')
+        print(f'\n{itemId}--> mongoDB入库成功'+'='*30)
 
     def run(self):
         tme = str(time.time()).replace('.', '')[0:13]
@@ -178,27 +191,29 @@ class TaoBao:
 if __name__ == "__main__":
     # 搜索配置
     # 搜索内容
-    str_searchContent = '裤子'
+    str_searchContent = '连衣裙'
     # 每页显示数量
     num_pageSize = 100
+    # 淘宝评论爬取数量，（天猫理论上默认是所有评论）
+    taobao_comments_num = 100
     # 从第一页 至 第几页（理论上可穷尽阿里服务器），推荐填入 1~100 ，页数再大则显示的内容匹配度不足
     num_page = 2
     # 阿里服务编号，12574478 固定不要更改，如菜鸟裹裹为 12574478 固定
     appKey = '12574478'
     # 储存--------------------------------------------
-    switch_save = 0  # 本地 csv 存储
-    # switch_save = 1 # mysql 存储
+    # switch_save = 0  # 本地 csv 存储
+    switch_save = 1  # mysql 存储
     # switch_save = 2 # mongoDB 存储
 
     # 开启线程的数量
     threads_num_get_pages = 1  # 抓取搜索页的线程数, 默认为 1, 建议也为1
-    threads_num_get_comments = 3  # 抓取评论页的线程数,当为 0 时,不抓取详情页面(评论)； -->待开发完善
+    threads_num_get_comments = 2  # 抓取评论页的线程数,当为 0 时,不抓取详情页面(评论)； -->待开发完善
 
     # 代理的设置
     proxies = {
-        "http": "",
-        "https": '',
+        'http': 'http://103.233.152.140:80',
+        # "https": '',
     }
 
     TaoBao(str_searchContent, num_pageSize, num_page, appKey,
-           threads_num_get_pages, switch_save, proxies, threads_num_get_comments)
+           threads_num_get_pages, switch_save, proxies, threads_num_get_comments, taobao_comments_num)
